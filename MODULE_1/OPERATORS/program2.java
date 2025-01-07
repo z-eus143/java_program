3ebc6f4bb25af5bd6d145cf0d98d19e69080ea8b
Tanmay Maji <tmaji435@gmail.com>
2025-01-07 13:21:09 +0530

    first commit

---
+++ MODULE_1/OPERATORS/program2.java
@@ -0,0 +1,20 @@
+package MODULE_1.OPERATORS;
+
+public class program2 {
+    public static void main(String[] args) {
+        int i1 = 10;
+        String s1 = "Hello";
+        System.out.println(i1 + s1); //10Hello
+        boolean b1 = true;
+        String s2 = "Hello";
+        int i2 = 10;
+        System.out.println(b1 + s2 + i2); //trueHello10
+        int hi = 10;
+        String hii = "hello";
+        int i4 = 10;
+        String S = "hi";
+        int s = 10;
+        System.out.println(10 + 20 + hii + i4 + S + hi + s + 10); //30hello10hi101010
+
+    }
+}
