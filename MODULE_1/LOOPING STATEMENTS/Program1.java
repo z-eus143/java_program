7a94f05c25aff43f6168d139b0c350980ed4c5a1
Tanmay Maji <tmaji435@gmail.com>
2025-01-21 09:37:41 +0530

    21-01-2025

---
+++ MODULE_1/LOOPING STATEMENTS/Program1.java
@@ -0,0 +1,9 @@
+public class Program1 {
+    public static void main(String[] args) {
+        int count = 0;
+        while(count < 5) {
+            System.out.println("*");
+            count++;
+        }
+    }
+}
---
+++ MODULE_1/LOOPING STATEMENTS/Program2.java
@@ -0,0 +1,10 @@
+public class Program2 {
+    public static void main (String[] args) {
+        int i = 10;
+        int count = 1;
+        while(count <= 10){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
