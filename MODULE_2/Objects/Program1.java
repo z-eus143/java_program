6fa4af10c22a4a279effd01911a601118d8527b9
Tanmay Maji <tmaji435@gmail.com>
2025-02-05 09:46:25 +0530

    05-02-2025

---
+++ MODULE_2/Objects/Program1.java
@@ -0,0 +1,18 @@
+package Objects;
+
+public class Program1 {
+
+    String title;
+    String auther;
+    int price;
+
+    public static void main(String[] args) {
+        Program1 b = new Program1();
+        b.title = "IKIGAI";
+        b.auther = "HECTOR GARCIA";
+        b.price = 375;
+        System.out.println("Title : " + b.title);
+        System.out.println("Auther : " + b.auther);
+        System.out.println("Price : " + b.price); 
+    }
+}
