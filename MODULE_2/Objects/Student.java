93604118d3cb7ee412160be30db889a718958467
Tanmay Maji <tmaji435@gmail.com>
2025-02-14 09:55:41 +0530

    "14-02-2025"

--- MODULE_2/Objects/Student.java
+++ MODULE_2/Objects/Student.java
@@ -2,7 +2,14 @@ public class Student
 {
     int id;
     String name;
+    String std;
 
     static String college;
     
+
+    public Student(int id,String name,String std){
+        this.id = id;
+        this.name = name;
+        this.std = std;
+    }
 }
--- MODULE_2/Objects/StudentDriver.java
+++ MODULE_2/Objects/StudentDriver.java
@@ -3,29 +3,19 @@ public class StudentDriver
     public static void main(String[] args) 
     {
 
-        Student s1 = new Student();
-        s1.id = 1;
-        s1.name = "SAHIL KHAN";
-        s1.college = "ISI";
+        Student s1 = new Student(1,"SAHIL KHAN","FY");
+        Student.college = "ISI";
         System.out.println(s1.id);
         System.out.println(s1.name);
         System.out.println(Student.college);
 
         System.out.println("======================================");
 
-        Student s2 = new Student();
-        s2.id = 2;
-        s2.name = "SIDDESH SHAIKH";
-        s2.college = "IIT-B";
+        Student s2 = new Student(2,"SIDDESH SHAIKH","SY");
+        Student.college = "IIT-B";
         System.out.println(s2.id);
         System.out.println(s2.name);
-        System.out.println(s2.college);
-
-
-        Student s3 = new Student();
-        System.out.println(s3.college);
-        System.out.println(s1.college);
-        
+        System.out.println(Student.college);
     }
     
 }
